d5da081b0827e437080443ae5cfbf9702583cbde
zhaijiayu572 <zjypersonal@163.com>
2016-11-26 19:28:55 +0800

    完善了组合轮播图

--- 组合轮播图/demo.js
+++ 组合轮播图/demo.js
@@ -3,13 +3,16 @@
     var oSlideBar = document.getElementById('slide-bar');
     var aSlideImg = oSlideBar.getElementsByTagName("img");
     var oContent = document.getElementById('content');
-    var aConImg = oContent.getElementsByTagName('img');
+    var aConDiv = oContent.getElementsByTagName('div');
     var oPrev = document.getElementById('prev');
     var oNext = document.getElementById('next');
-    var iHeight = aConImg[0].offsetHeight;
+    for(i=0;i<aConDiv.length;i++){
+        aConDiv[i].style.background = "url('img/"+(i+1)+".jpg')";
+    }
+    var iHeight = aConDiv[0].offsetHeight;
     var iSlideMax = aSlideImg[0].offsetWidth*(aSlideImg.length-3);
     var iNow = 0;
-    var iNowIndex = aConImg.length;
+    var iNowIndex = aConDiv.length;
     for(var k=0;k<aSlideImg.length;k++){
         aSlideImg[k].onmouseover = function () {
             this.className = "selected";
@@ -19,7 +22,7 @@
             aSlideImg[iNow].className = 'selected';
         }
     }
-    oContainer.onmouseover =function (e) {
+    oContainer.onmouseover =function () {
 
         oContainer.onmousemove =function (e) {
             e = e || window.event;
@@ -28,32 +31,32 @@
                 isLeft = true;
             }
             if(isLeft){
-                oPrev.style.display = "block";
-                oNext.style.display = 'none';
+                animate(oPrev,{opacity:100});
+                animate(oNext,{opacity:0});
             }else{
-                oNext.style.display = "block";
-                oPrev.style.display = 'none';
+                animate(oNext,{opacity:100});
+                animate(oPrev,{opacity:0});
             }
         }
 
     };
     oContainer.onmouseout = function () {
-        oPrev.style.display = 'none';
-        oNext.style.display = 'none';
+        animate(oPrev,{opacity:0});
+        animate(oNext,{opacity:0});
     };
-    for(var i=0;i<aConImg.length;i++){
-        aConImg[i].style.zIndex = aConImg.length-i;
+    for(var i=0;i<aConDiv.length;i++){
+        aConDiv[i].style.zIndex = aConDiv.length-i;
     }
     function changeImg(index) {
         for(var j=0;j<aSlideImg.length;j++){
             aSlideImg[j].className = '';
         }
         aSlideImg[index].className = 'selected';
-        var oConImg = aConImg[index];
-        console.log(oConImg);
-        oConImg.style.height = 0;
-        oConImg.style.zIndex = ++iNowIndex;
-        animate(oConImg,{height:iHeight});
+        var oConDiv = aConDiv[index];
+        console.log(oConDiv);
+        oConDiv.style.height = 0;
+        oConDiv.style.zIndex = ++iNowIndex;
+        animate(oConDiv,{height:iHeight});
         var iLeft = -aSlideImg[0].offsetWidth*(index-1<0?0:index-1) ;
         if(iLeft < -iSlideMax){
             iLeft = -iSlideMax;
@@ -63,7 +66,7 @@
     }
     oNext.onclick = function () {
         iNow++;
-        if(iNow>aConImg.length-1){
+        if(iNow>aConDiv.length-1){
             iNow = 0;
         }
         console.log(iNow);
@@ -72,7 +75,7 @@
     oPrev.onclick = function () {
         iNow--;
         if(iNow<0){
-            iNow = aConImg.length-1;
+            iNow = aConDiv.length-1;
         }
         changeImg(iNow);
     }
